fbd923126583de416f5ce486c24c9eb2eb4a503d
ander94ds <ander94ds@gmail.com>
2021-12-20 16:30:15 -0400

    data naigation flow showed to willber

---
+++ controllers/usersController.js
@@ -0,0 +1,9 @@
+const res = require("express/lib/response")
+
+exports.login = function(req,res){
+    res.render('login')
+}
+
+exports.home = function(req,res){
+    res.render('login')
+}
--- router.js
+++ router.js
@@ -1,7 +1,11 @@
 // Declaración de variables de condición inicial
 const express = require('express')
-
 const router = express.Router()
+const userController = require('./controllers/usersController')
+
+router.get('/', userController.login)
+
+router.post('/home', userController.home)
 
 
 
